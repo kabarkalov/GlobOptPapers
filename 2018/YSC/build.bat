d2d0e321bdc459789a6fe150a19b64d69539a79b
sovrasov.v <sovrasov.v@255e1d6d-7611-4961-ab30-28fe885dcc59>
2018-05-13 09:25:08 +0000

    YSC: add a build script for windows

---
+++ 2018/YSC/build.bat
@@ -0,0 +1,9 @@
+pdflatex ysc_paper.tex
+bibtex ysc_paper.aux
+pdflatex ysc_paper.tex
+mv ysc_paper.pdf .\build\
+rm ysc_paper.log
+rm ysc_paper.bbl
+rm ysc_paper.out
+rm ysc_paper.blg
+rm ysc_paper.aux
