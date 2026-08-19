a0dc8d07c91ed949c47a82ae3a6bcfbc3fea78de
Konstantin Barkalov <barkalov@vmk.unn.ru>
2026-08-19 14:58:01 +0300

    delete old version

--- 2026/PAVT/make.bat
+++ 2026/PAVT/make.bat
@@ -1,5 +1,5 @@
-pdflatex -synctex=1 paper_1.tex
-biber paper_1
-pdflatex -synctex=1 paper_1.tex
-pdflatex -synctex=1 paper_1.tex
+pdflatex -synctex=1 paper.tex
+biber paper
+pdflatex -synctex=1 paper.tex
+pdflatex -synctex=1 paper.tex
 
